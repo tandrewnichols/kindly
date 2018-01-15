734af2e7fa544c54df8afe08182b85d11562c7ca
Andrew Nichols <tandrewnichols@gmail.com>
2018-01-15 12:24:16 -0500

    Test error scenarios

--- test/kindly.js
+++ test/kindly.js
@@ -29,58 +29,109 @@ describe('kindly', () => {
 
   context('async', () => {
     context('without noFollow', () => {
-      it('should return a manifest of files', (done) => {
-        kindly.get('test/fixtures').then(results => {
+      it('should return a manifest of files', () => {
+        return kindly.get('test/fixtures').then(results => {
           results.should.eql({
             files: ['test/fixtures/bar.js', 'test/fixtures/foo.js', 'test/fixtures/hello.js'],
             directories: ['test/fixtures/baz'],
             symlinks: [],
             other: []
           });
-          done();
-        }, done);
+        });
       })
     })
 
     context('with noFollow', () => {
-      it('should return a manifest of files', (done) => {
-        kindly.get('test/fixtures', { noFollow: true }).then(results => {
+      it('should return a manifest of files', () => {
+        return kindly.get('test/fixtures', { noFollow: true }).then(results => {
           results.should.eql({
             files: ['test/fixtures/bar.js', 'test/fixtures/foo.js'],
             directories: ['test/fixtures/baz'],
             symlinks: ['test/fixtures/hello.js'],
             other: []
           });
-          done();
-        }, done);
+        });
       })
     })
 
     context('with a callback and no opts', () => {
       it('should return a manifest of files', (done) => {
-        kindly.get('test/fixtures', results => {
+        kindly.get('test/fixtures', (err, results) => {
+          if (err) {
+            return done(err);
+          }
+
           results.should.eql({
             files: ['test/fixtures/bar.js', 'test/fixtures/foo.js', 'test/fixtures/hello.js'],
             directories: ['test/fixtures/baz'],
             symlinks: [],
             other: []
           });
+
           done();
-        }, done);
+        });
       })
     })
 
     context('with a callback and opts', () => {
       it('should return a manifest of files', (done) => {
-        kindly.get('test/fixtures', { noFollow: true }, results => {
+        kindly.get('test/fixtures', { noFollow: true }, (err, results) => {
+          if (err) {
+            return done(err);
+          }
+
           results.should.eql({
             files: ['test/fixtures/bar.js', 'test/fixtures/foo.js'],
             directories: ['test/fixtures/baz'],
             symlinks: ['test/fixtures/hello.js'],
             other: []
           });
+
           done();
-        }, done);
+        });
+      })
+    })
+
+    context('an error', () => {
+      const sinon = require('sinon');
+      const fs = require('fs-extra');
+
+      const generateError = () => {
+        return new Promise((resove, reject) => reject('An error'));
+      };
+
+      context('reading files', () => {
+        beforeEach(() => {
+          let readdir = sinon.stub(fs, 'readdir');
+          readdir.callsFake(generateError);
+        })
+
+        afterEach(() => {
+          fs.readdir.restore();
+        })
+
+        it('should handle handle the error', () => {
+          return kindly.get('test/fixtures').catch(err => {
+            err.should.eql('An error');
+          });
+        })
+      })
+
+      context('statting files', () => {
+        beforeEach(() => {
+          let stat = sinon.stub(fs, 'stat');
+          stat.callsFake(generateError);
+        })
+
+        afterEach(() => {
+          fs.stat.restore();
+        })
+
+        it('should handle the error', () => {
+          return kindly.get('test/fixtures').catch(err => {
+            err.should.eql('An error');
+          });
+        })
       })
     })
   })
